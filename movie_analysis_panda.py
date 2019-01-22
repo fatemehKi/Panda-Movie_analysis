e16ecff33bdf2ffabd7cd03ad655119b8f4c4bbc
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-22 15:19:07 -0500

    Update and rename movie_analysis_panda to movie_analysis_panda.py

---
+++ movie_analysis_panda.py
@@ -0,0 +1,11 @@
+#author: Fatemeh
+import pandas as pd
+
+!ls "C:\kiaie\Data Science and ML\Edx-python for DS\\movielens"
+
+#see the word count line.. number of movies.. UNIX commands with !
+!cat ./movielens/movies.csv | wc -l
+
+movies = pd.read_csv('./movielens/movies.csv', sep=',')
+print(type(movies))
+movies.head(15)
